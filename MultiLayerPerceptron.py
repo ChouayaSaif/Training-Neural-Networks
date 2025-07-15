df4b3676f559c3ed680095a034911f95c480ab27
Saif3r <saif.chouaya@tbs.u-tunis.tn>
2025-07-15 15:03:37 +0200

    running an MLP

--- MultiLayerPerceptron.py
+++ MultiLayerPerceptron.py
@@ -39,13 +39,50 @@ class MultiLayerPerceptron:
         self.network = []  # The list of lists of neurons
         self.values = []  # The list of lists of output values
 
+        for i in range(len(self.layers)):
+            self.values.append([])
+            self.network.append([])
+            self.values[i] = [0.0 for j in range(self.layers[i])]
+            if i > 0:  # network[0] is the input layer, so it has no neurons
+                for j in range(self.layers[i]):
+                    self.network[i].append(Perceptron(inputs=self.layers[i - 1], bias=self.bias))
+
         self.network = np.array([np.array(x) for x in self.network], dtype=object)
         self.values = np.array([np.array(x) for x in self.values], dtype=object)
 
+    def set_weights(self, w_init):
+        """Set the weights.
+           w_init is a 3D list with the weights for all but the input layer."""
+        for i in range(len(w_init)):
+            for j in range(len(w_init[i])):
+                self.network[i + 1][j].set_weights(w_init[i][j])
+
+    def print_weights(self):
+        print()
+        for i in range(1, len(self.network)):
+            for j in range(self.layers[i]):
+                print("Layer", i + 1, "Neuron", j, self.network[i][j].weights)
+        print()
+
+    def run(self, x):
+        """Feed a sample x into the MultiLayer Perceptron."""
+        x = np.array(x, dtype=object)
+        self.values[0] = x
+        for i in range(1, len(self.network)):
+            for j in range(self.layers[i]):
+                self.values[i][j] = self.network[i][j].run(self.values[i - 1])
+        return self.values[-1]
 
 
 
-
+mlp = MultiLayerPerceptron(layers=[2,2,1])  #mlp
+mlp.set_weights([[[-10,-10,15],[15,15,-10]],[[10,10,-15]]])
+mlp.print_weights()
+print("MLP:")
+print ("0 0 = {0:.10f}".format(mlp.run([0,0])[0]))
+print ("0 1 = {0:.10f}".format(mlp.run([0,1])[0]))
+print ("1 0 = {0:.10f}".format(mlp.run([1,0])[0]))
+print ("1 1 = {0:.10f}".format(mlp.run([1,1])[0]))
 
 
 
